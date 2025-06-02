712e78d955d16d3b2e62d3472b4039f19c5143d4
Gabriel <gabriellourencofaria@gmail.com>
2025-06-02 19:06:29 -0300

    função clear e limpar + deleção manual de uma tarefa específica

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 
 let Savetarefas = []
+window.addEventListener("beforeunload",function(event){
+    localStorage.setItem("minhastarefas", JSON.stringify(Savetarefas))
+}) 
 window.onload = function(){
-    let historico= JSON.parse(localStorage.getItem('minhastarefas'))
+    // SE QUIZER MARCAR COMO CONCLUIDO E APAGAR AUTOMATICO
+/*     const checkboxes= document.querySelectorAll('.tarefa')
+    checkboxes.forEach(checkbox=>{
+        checkbox.addEventListener("change", function(){
+            if(this.checked){
+                deletar(this)
+                console.log('deu')
+            }else{
+                console.log('nao')
+            }
+        })
+            
+    }) */
+    let historico= JSON.parse(localStorage.getItem('minhastarefas')) || []
     Savetarefas= historico
     let numTaref= historico.length
     for (let i=0; i<=numTaref-1; i++){
         let res = document.getElementById('list')
         let tarefa= document.createElement('ul')
         let texto= historico[i]
-        tarefa.innerHTML=`<p>${texto}</p> <input type="checkbox" name="nada" class="tarefa"> <br>`
+        tarefa.innerHTML=`<p>${texto}</p> <input type="checkbox" name="checkbox" class="tarefa"> <br> <button id="${texto}" onclick="deletar(this)" class="delete">🗑️</button>`
         res.appendChild(tarefa)
 
-        console.log(historico)
+// pode-se fazer um foreach 
     }
 }
 
@@ -20,18 +36,65 @@ function adicionar(){
     let res = document.getElementById('list')
     let tarefa= document.createElement('ul')
     let texto= document.getElementById('text').value
+    if(texto.trim()==""){
+        return
+    }
     guardarTarefa(texto)
-    tarefa.innerHTML=`<p>${texto}</p> <input type="checkbox" name="nada" class="tarefa"> <br>`
+    tarefa.innerHTML=`<p>${texto}</p> <input type="checkbox" name="checkbox" class="tarefa"><button id="${texto}" onclick="deletar(this)" class="delete">🗑️</button> <br>`
     res.appendChild(tarefa)
+    document.getElementById('text').value=""
+
+    // SE QUIZER MARCAR COMO CONCLUIDO E APAGAR AUTOMATICO
+/*     const checkboxes= document.querySelectorAll('.tarefa')
+    checkboxes.forEach(checkbox=>{
+    checkbox.addEventListener("change", function(){
+        if(this.checked){
+            deletar(this)
+            console.log('deu')
+        }else{
+            console.log('nao')
+        }
+    })
+        
+}) */
+
 
 }
 function guardarTarefa(texto){
     Savetarefas.push(texto)
-    console.log(Savetarefas)
-    localStorage.setItem('minhastarefas', JSON.stringify(Savetarefas))
 
 }
-function limpar(){
-    Savetarefas = []
-    historico= []
+// MARCAR A CHECKBOX
+const checkboxes= document.querySelectorAll('.tarefa')
+checkboxes.forEach(checkbox=>{
+    checkbox.addEventListener("change", function(){
+        if(this.checked){
+            deletar(this)
+            console.log('deu')
+        }else{
+            console.log('nao')
+        }
+    })
+        
+})
+
+
+
+function deletar(task){
+    task.parentElement.remove()
+    const index = Savetarefas.findIndex((conteudo)=> conteudo===task.id)
+    if (index!==-1){
+        Savetarefas.splice(index, 1)
+    }
+    console.log(Savetarefas)
 }
+function limpar(){
+    localStorage.clear()
+    Savetarefas=[]
+    console.log(localStorage)
+    document.getElementById('list').innerHTML=""
+
+}
+
+// usar foreach no onload
+// colocar função de editar 
